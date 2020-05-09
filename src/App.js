bc65984f7be9574c76ac0048747ead22b92fe7aa
Alexander Kras'ko <0m3r.mail@gmail.com>
2020-05-09 14:27:41 +0300

    done

--- src/App.js
+++ src/App.js
@@ -5,22 +5,9 @@ import withFetch from './hoc/withFetch';
 import withFilter from './hoc/withFilter';
 
 class App extends Component {
-  state = {
-    filter: ""
-  };
-
-  handleChange = ({ target }) => this.setState({ filter: target.value });
 
   get getResults() {
-    let { filter } = this.state;
-    let { results } = this.props;
-
-    if (filter) {
-      results = results.filter(item =>
-        item.name.toLowerCase().includes(filter.toLowerCase())
-      );
-    }
-    return <PersonList results={results} />;
+    return <PersonList results={this.props.filteredResults()} />;
   }
 
   render() {
@@ -28,7 +15,7 @@ class App extends Component {
       <div className="container pt-3">
         <div className="row">
           <div className="col-md-3">
-            <Filter handleChange={this.handleChange} />
+            <Filter handleChange={this.props.handleChange} />
           </div>
           <div className="offset-md-1 col-md-4">{this.getResults}</div>
         </div>
@@ -39,5 +26,4 @@ class App extends Component {
 
 const apiUrl = 'https://jsonplaceholder.typicode.com/users';
 
-// export default withFetch(apiUrl)(App);
-export default withFilter()(withFetch(apiUrl)(App));
+export default withFetch(apiUrl)(withFilter()(App));
--- src/hoc/withFilter.js
+++ src/hoc/withFilter.js
@@ -1,27 +1,39 @@
 import React, { Component } from "react";
 
-const withFilter = () => (BaseComponent) {
+const withFilter = () => BaseComponent => {
   class WithFilterClass extends Component {
-    // constructor(props) {
-    //   super(props);
-    //   this.state = {
-    //     results: []
-    //   };
-    // }
+    constructor(props) {
+      super(props);
+      this.state = {
+        filter: ''
+      };
+    }
 
-    // async componentDidMount() {
-    //   const response = await fetch(apiUrl);
-    //   const results = await response.json();
-    //   this.setState({ results });
-    // }
+    handleChange = ({ target }) => this.setState({ filter: target.value });
+
+    filteredResults = () => {
+      let { filter } = this.state;
+      let { results } = this.props;
+
+      if (filter) {
+        results = results.filter(item =>
+          item.name.toLowerCase().includes(filter.toLowerCase())
+        );
+      }
+      return results;
+    };
 
     render() {
       return (
-          <BaseComponent {...this.state} {...this.props} />
+          <BaseComponent
+            {...this.props}
+            {...this.state}
+            handleChange={this.handleChange}
+            filteredResults={this.filteredResults}
+          />
       );
     }
   }
-
   return WithFilterClass;
 }
 
